48de969675b841791a533a48b318f8e92e0dfec5
michelangelomancini <michelangelomancini8@gmail.com>
2025-02-27 17:00:11 +0100

    consegna MILESTONE 3 (aggiunta backgorund card e output voto con stelle)

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,26 @@
 // funzione per generare lista film
 function MovieList(props) {
+
+    // funzione per calcolare le stelle con icone di Fontawesome
+    function getStars(vote) {
+
+        // calcolo per passare ad un rating più compatto ed arrotondato per eccesso
+        const stars = Math.ceil(vote / 2);
+        const starsArray = [];
+
+        // Aggiungo le stelle piene
+        for (let i = 0; i < stars; i++) {
+            starsArray.push(<i key={i} className="fas fa-star"></i>);
+        }
+
+        // Aggiungo le stelle vuote
+        for (let i = stars; i < 5; i++) {
+            starsArray.push(<i key={i} className="far fa-star"></i>);
+        }
+
+        return starsArray;
+    }
+
     return (
 
         <div>
@@ -10,11 +31,17 @@ function MovieList(props) {
 
                 // se ci sono film corrispondenti genero l'output con le info del film mappando l'oggetto ricevuto in prop
                 props.movies.map((movie) => (
-                    <div key={movie.id}>
+
+                    <div
+                        key={movie.id}
+                        className="movie-card"
+                        style={{
+                            backgroundImage: `url(https://image.tmdb.org/t/p/w342${movie.poster_path})`,
+                        }}
+                    >
                         <h3>{movie.title || movie.name}</h3>
                         <p>Titolo originale:{movie.original_title || movie.original_name}</p>
-                        <img src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.original_language} />
-                        <p>Voto: {movie.vote_average}</p>
+                        <p>Voto: {getStars(movie.vote_average)}</p>
                     </div>
                 ))
             ) : (
