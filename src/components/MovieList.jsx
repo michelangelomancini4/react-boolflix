54d3860c63d0f3979dce58f67ff6c54cb6d68305
michelangelomancini <michelangelomancini8@gmail.com>
2025-02-28 10:30:15 +0100

    creazione MovieCard e conseguente fixing dell'inserimento bandierine associate alla lingua

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,55 +1,19 @@
-// funzione per generare lista film
-function MovieList(props) {
+import MovieCard from "./MovieCard";
 
-    // funzione per calcolare le stelle con icone di Fontawesome
-    function getStars(vote) {
-
-        // calcolo per passare ad un rating più compatto ed arrotondato per eccesso
-        const stars = Math.ceil(vote / 2);
-        const starsArray = [];
-
-
-        // Aggiungo le stelle piene
-        for (let i = 0; i < stars; i++) {
-            starsArray.push(<i key={i} className="fas fa-star"></i>);
-        }
-
-        // Aggiungo le stelle vuote
-        for (let i = stars; i < 5; i++) {
-            starsArray.push(<i key={i} className="far fa-star"></i>);
-        }
-
-        return starsArray;
-    }
-
+function MovieList({ movies }) {
     return (
-
         <div>
             <h2>Risultati:</h2>
+            {/* controllo se ci sono film o serie tv corrispondenti alla ricerca */}
+            {movies.length > 0 ? (
 
-            {/* controllo se ci sono film corrispondenti */}
-            {props.movies.length > 0 ? (
-
-                // se ci sono film corrispondenti genero l'output con le info del film mappando l'oggetto ricevuto in prop
-                props.movies.map((movie) => (
-
-                    <div
-                        key={movie.id}
-                        className="movie-card"
-                        style={{
-                            backgroundImage: `url(https://image.tmdb.org/t/p/w342${movie.poster_path})`,
-                        }}
-                    >
-                        <h3>{movie.title || movie.name}</h3>
-                        <p>Titolo originale:{movie.original_title || movie.original_name}</p>
-                        <p>Voto: {getStars(movie.vote_average)}</p>
-                        <img src="../public/images/{movie.original_language}.jpg" alt="" />
-                    </div>
-                ))
-            ) : (
-                // output generato nel caso non ci siano film corrispondenti alla ricerca
-                <p>Ci dispiace! Nessun film trovato!</p>
-            )}
+                // itero con map il prop ricevuto, e per ogni iterazione creo una MovieCard e lo dono come prop a MovieCard.jsx
+                movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+            ) :
+                (
+                    // output nel caso non si trovino film o serie tv corrispondenti alla ricerca
+                    <p>Ci dispiace! Nessun film trovato!</p>
+                )}
         </div>
     );
 }
